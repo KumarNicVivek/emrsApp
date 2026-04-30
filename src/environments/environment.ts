9d5dc3489094bf9015983124d7bef8810094f0d2
KumarNicVivek <kumarnicvivek@gmail.com>
2026-04-30 16:52:43 +0530

    Add candidate API base URL to environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
     webapibaseUrl : 'http://localhost:5000',
     dokerwebapibaseUrl : 'http://localhost:5100',
     authbaseUrl : 'http://localhost:5049',
-    dockerbaseUrl : 'http://localhost:7000'
+    dockerbaseUrl : 'http://localhost:7000',
+    candidateapibaseUrl : 'https://emrswebapp-azatf4h9e3a6gfcs.canadacentral-01.azurewebsites.net',
 };
